eb05af484b38702135bea5f0537d14a738d042bd
eymard <francisco.eymard.oliveira@gmail.com>
2023-02-27 23:10:11 -0300

    create supportsWorksType function for validade if browser support is

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -1,10 +1,17 @@
 import Camera from "../../../lib/shared/camera.js"
+import { supportsWorksType } from "../../../lib/shared/util.js"
 import VideoPlayerController from "./controller.js"
 import VideoPlayerService from "./service.js"
 import VideoPlayerView from "./view.js"
 
 
 const camera = await Camera.init()
+
+if (supportsWorksType()) {
+  console.log('Suport')
+} else {
+  console.log('Nop');
+}
 
 const videoPlayerWorker = new Worker('./src/worker.js')
 const [rootPath] = window.location.href.split('/pages/')
