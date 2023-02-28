8bb0d5bc9d91154516c389458652fdb369f5f673
eymard <francisco.eymard.oliveira@gmail.com>
2023-02-27 22:40:16 -0300

    add webcam in the browser

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -1,6 +1,10 @@
+import Camera from "../../../lib/shared/camera.js"
 import VideoPlayerController from "./controller.js"
 import VideoPlayerService from "./service.js"
 import VideoPlayerView from "./view.js"
+
+
+const camera = await Camera.init()
 
 const videoPlayerWorker = new Worker('./src/worker.js')
 const [rootPath] = window.location.href.split('/pages/')
