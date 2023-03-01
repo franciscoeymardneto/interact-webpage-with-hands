3e3224a277c32e2142f2dc141241409db64ebcb9
eymard <francisco.eymard.oliveira@gmail.com>
2023-02-28 22:00:03 -0300

    add handpose confug

--- pages/titles/src/services/handGestureService.js
+++ pages/titles/src/services/handGestureService.js
@@ -1,5 +1,25 @@
 export default class HandGestureService {
-    constructor () {
-        
+    #fingerpose
+    #handPoseDetection
+    #handPoseVersion
+    #detector = null
+    constructor({ fingerpose, handPoseDetection, handPoseVersion }) {
+        this.#fingerpose = fingerpose
+        this.#handPoseDetection = handPoseDetection
+        this.#handPoseVersion = handPoseVersion
+    }
+
+    async initializeDetector() {
+        if(this.#detector) return this.#detector;
+
+        const model = this.#handPoseDetection.SupportedModels.MediaPipeHands;
+        const detectorConfig = {
+            runtime: 'mediapipe', // or 'tfjs',
+            solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handPoseVersion}`,
+            // full is the most hard and precisely
+            modelType: 'lite',
+            maxHands: 2
+        }
+        this.#detector = await this.#handPoseDetection.createDetector(model, detectorConfig);
     }
 }
