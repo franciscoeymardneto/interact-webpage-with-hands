ae74390f27cd7ba605633c3b7549072656d243b0
eymard <francisco.eymard.oliveira@gmail.com>
2023-03-01 22:06:39 -0300

    add canvas methods

--- pages/titles/src/views/handGestureView.js
+++ pages/titles/src/views/handGestureView.js
@@ -1,7 +1,19 @@
 export default class HandGestureView {
-    constructor () {
-        
-    }
+  #handsCanvas = document.querySelector('#hands')
+  #canvasContext = this.#handsCanvas.getContext('2d')
+
+  constructor(){
+    this.#handsCanvas.width = globalThis.screen.availWidth
+    this.#handsCanvas.height = globalThis.screen.availHeight
+  }
+
+  clear() {
+    this.#canvasContext.clearRect(0, 0, this.#handsCanvas.width, this.#handsCanvas.height)
+  }
+
+  drawResults(hands) {
+
+  }
 
     loop (fn) {
         requestAnimationFrame(fn)
